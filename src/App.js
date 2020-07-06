cfaf508f556a406e067f02016e19af5646418ccb
TANI <chantani0811@gmail.com>
2020-07-05 18:33:47 -0700

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
 import './App.css';
-import Directory from './components/DirectoryComponent';
-import { CAMPSITES } from './shared/campsites';
+
 /*function App() {
   return (
     <div className="App">
@@ -25,23 +24,11 @@ import { CAMPSITES } from './shared/campsites';
 }
 */
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-       campsites : CAMPSITES
-
-    };
-}
   render() {
       return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} />
-          </div>
+        <div className="App">
+          <Main />
+        </div>
       );
   }
 }
