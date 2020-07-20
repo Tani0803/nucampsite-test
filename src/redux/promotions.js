b0e853211c126690b51e2c1a8d8ff250807f530d
TANI <chantani0811@gmail.com>
2020-07-19 23:50:46 -0700

    Combining Reducers

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,8 +1,8 @@
 import { PROMOTIONS } from '../shared/promotions';
 
-export const Promotion = (state = PROMOTIONS, action) => {
+export const Promotions = (state = PROMOTIONS, action) => {
     switch (action.type) {
         default:
-            return state;
-    }
+          return state;
+      }
 };
